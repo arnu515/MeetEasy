edfa4da98eaa165d1b89e449e58c1aa628dd0850
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-06-23 15:23:59 +0530

    fix: firstOrNull

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -3,12 +3,13 @@ import { getSession, type SessionData } from "./session"
 import db from './db'
 import { eq } from 'drizzle-orm'
 import { users } from './db/schema'
+import { firstOrNull } from "./utils"
 
 export async function getUser(session?: IronSession<SessionData>) {
 	const ssn = session || await getSession()
 	if (!ssn.userId) return null
 	try {
-		return await db.select().from(users).where(eq(users.id, ssn.userId!))
+		return firstOrNull(await db.select().from(users).where(eq(users.id, ssn.userId!)))
 	} catch(e) {
 		console.error("Could not get user: ", e)
 		return null
