4d8294ccf2744cc0f13d63afac4342893357d3e0
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-06-23 08:02:53 +0530

    feat: add

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function firstOrNull<T=any>(arr: T[]): T | null {
+  return arr[0] ?? null
+}
+
+export const NEXT_REDIRECT_ERROR_MESSAGE = "NEXT_REDIRECT";
