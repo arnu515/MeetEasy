a5d696f68216cea981282a9abfa7f26a24a0f8d5
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-06-23 14:46:35 +0530

    feat: add getTimezoneOffset function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,4 +9,16 @@ export function firstOrNull<T = any>(arr: T[]): T | null {
 	return arr[0] ?? null
 }
 
+export function getTimeZoneOffset(argOffsetMin?: number) {
+	const offsetMin = argOffsetMin ?? new Date().getTimezoneOffset()
+	if (offsetMin === 0) return 'UTC'
+	const offsetHours = Math.floor(Math.abs(offsetMin) / 60)
+	let offset = "UTC"
+	// flip the signs, since UTC is checked against local, not the other way around
+	if (offsetMin < 0) offset += "+"
+	else offset += "-"
+	offset += `${offsetHours}:${(Math.abs(offsetMin) - (offsetHours * 60)).toString().padStart(2, '0')}`
+	return offset
+}
+
 export const NEXT_REDIRECT_ERROR_MESSAGE = 'NEXT_REDIRECT'
